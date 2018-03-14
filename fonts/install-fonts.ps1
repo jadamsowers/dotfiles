3f454acf992ba50d2bbcabd1b78c870490882348
J. Adam Sowers <adam.sowers@gmail.com>
2018-03-14 15:51:18 -0400

    Added PoSh script to install Powerline fonts. The script installs all fonts listed in `fontlist.txt`.

---
+++ fonts/install-fonts.ps1
@@ -0,0 +1,16 @@
+function Get-ScriptDirectory 
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+$fontsDir = "$($env:USERPROFILE)\fonts"
+$fontsToInstall = get-content (join-path (Get-ScriptDirectory) "fontlist.txt")
+
+git clone https://github.com/powerline/fonts.git $fontsDir
+
+$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14) # https://superuser.com/questions/201896/how-do-i-install-a-font-from-the-windows-command-prompt
+foreach ($font in $fontsToInstall)
+{
+    Get-ChildItem -Recurse -include *.ttf,*.otf $fontsDir/$font | % { $fonts.CopyHere($_.fullname) }
+}
